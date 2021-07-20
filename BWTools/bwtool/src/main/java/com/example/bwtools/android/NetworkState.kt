5199e11aed943cda8d3ce844a1f34a36b55b9530
BaeWooRam <41356481+BaeWooRam@users.noreply.github.com>
2021-07-20 09:21:58 +0900

    Update NetworkState.kt

--- BWTools/bwtool/src/main/java/com/example/bwtools/android/NetworkState.kt
+++ BWTools/bwtool/src/main/java/com/example/bwtools/android/NetworkState.kt
@@ -2,7 +2,7 @@ package com.example.bwtools.android
 
 sealed class NetworkState<out T> {
     object Init : NetworkState<Nothing>()
-    class Loading<T>(val type: ProgressType) : NetworkState<Nothing>()
+    class Loading<T>() : NetworkState<T>()
     class Success<T>(val data: T) : NetworkState<T>()
     class Failure<T>(val exception: Exception) : NetworkState<T>()
     class Complete<T>(val data: T) : NetworkState<T>()
